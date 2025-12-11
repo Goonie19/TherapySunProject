f4b03cf4158c83e8cdfff388c4671349fc890293
Goonie19 <antonio.espinosabarrios98@gmail.com>
2025-12-11 15:29:48 +0100

    Rotator controller added

---
+++ scripts/Minigame/Meteor/meteor_movement.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var speed : float = 201
+
+var direction: Vector2 =Vector2(0,-1)
+
+func _process(delta: float) -> void:
+	velocity = speed * direction
+	move_and_slide() 
+
+func initialize(new_speed: float, dir: Vector2):
+	speed = new_speed
+	direction = dir
---
+++ scripts/Minigame/Rotator/blocker_rotator.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var rotation_speed : float = 3
+
+var direction : float
+
+func _process(delta: float) -> void:
+	read_input()
+	
+	rotation += rotation_speed * delta * direction
+
+func setRotation(rotationDir:float):
+	direction = rotationDir
+
+func read_input():
+	var dir = 0
+	if Input.is_key_pressed(KEY_RIGHT):
+		dir += 1
+	if Input.is_key_pressed(KEY_LEFT):
+		dir -= 1
+	
+	setRotation(dir)
