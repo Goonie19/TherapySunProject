4eed4777eced31b627a2fbbf6dcc99120c4f6a22
Goonie19 <antonio.espinosabarrios98@gmail.com>
2026-03-17 23:16:35 +0100

    Audio System added

---
+++ scripts/AudioSystem/audio_controller.gd
@@ -0,0 +1,25 @@
+extends Node
+
+class_name AudioController
+
+const AUDIO_BUS_NAME = "Master"
+
+@onready var _bus := AudioServer.get_bus_index(AUDIO_BUS_NAME)
+@export var music_player : AudioStreamPlayer2D
+@export var sfx_player : AudioStreamPlayer2D
+
+var value : float
+
+func _ready() -> void:
+	value = db_to_linear(AudioServer.get_bus_volume_db(_bus))
+
+func play_music(music_stream: AudioStream) -> void:
+	music_player.stream = music_stream
+	music_player.play()
+
+func play_sound(sfx_stream: AudioStream) -> void:
+	sfx_player.stream = sfx_stream
+	sfx_player.play()
+
+func set_volume(volume: float) -> void:
+	AudioServer.set_bus_volume_db(_bus, linear_to_db(volume))
