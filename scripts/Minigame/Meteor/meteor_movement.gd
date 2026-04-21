0c57cb84acd2f92c05f76ad7e2d1f9f5f82f2deb
Goonie19 <antonio.espinosabarrios98@gmail.com>
2026-04-21 12:48:06 +0200

    meteors now break with animation when hit a surface

--- scripts/Minigame/Meteor/meteor_movement.gd
+++ scripts/Minigame/Meteor/meteor_movement.gd
@@ -3,6 +3,7 @@ extends Node2D
 class_name MeteorMovement
 
 @export var rb: RigidBody2D
+@export var anim_tree: AnimationTree
 
 var speed : float = 201
 
@@ -17,4 +18,8 @@ func initialize(new_speed: float, dir: Vector2):
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	anim_tree.set("parameters/conditions/Break", true)
+	
+
+func destroy_meteor():
 	queue_free()
