c487fd82fbed8dc3fcfcdd14662bce6345345a6a
Goonie19 <antonio.espinosabarrios98@gmail.com>
2026-05-19 13:35:53 +0200

    crossfade for music done

--- scripts/AudioSystem/audio_controller.gd
+++ scripts/AudioSystem/audio_controller.gd
@@ -6,7 +6,10 @@ const AUDIO_BUS_NAME = "Master"
 
 @onready var _bus := AudioServer.get_bus_index(AUDIO_BUS_NAME)
 @export var music_player : AudioStreamPlayer2D
+@export var music_player_2 : AudioStreamPlayer2D
 @export var sfx_player : AudioStreamPlayer2D
+
+@export var animator : AnimationPlayer
 
 var value : float
 
@@ -14,8 +17,23 @@ func _ready() -> void:
 	value = db_to_linear(AudioServer.get_bus_volume_db(_bus))
 
 func play_music(music_stream: AudioStream) -> void:
-	music_player.stream = music_stream
-	music_player.play()
+	if  not music_player.playing and not music_player_2.playing:
+		music_player.stream = music_stream
+		music_player.play()
+		animator.play("FadeToTrack1")
+		return
+	
+	if music_player.playing :
+		music_player_2.stream = music_stream
+		music_player_2.play()
+		animator.play("FadeToTrack2")
+		return
+	
+	if music_player_2.playing :
+		music_player.stream = music_stream
+		music_player.play()
+		animator.play("FadeToTrack1")
+		return
 
 func play_sound(sfx_stream: AudioStream, random_pitch: bool = false) -> void:
 	sfx_player.stream = sfx_stream
